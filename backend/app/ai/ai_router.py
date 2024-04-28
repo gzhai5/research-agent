9e0220984b39b1504dfffd9d5e218b6e36594532
gz244 <gz244@cornell.edu>
2024-04-28 18:34:38 -0400

    add auth depends

--- backend/app/ai/ai_router.py
+++ backend/app/ai/ai_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from loguru import logger
 from app.ai.interfaces import AIInvokeRequest
 from app.ai.ai_service import ai_service
+from app.auth.utils.utils import get_current_user
 
 
 # define the router
@@ -10,7 +11,7 @@ router = APIRouter(prefix="/ai", tags=["ai"])
 
 # POST /ai/invoke
 @router.post("/invoke", response_description="invoke the AI model", status_code=200)
-def invoke_ai(body: AIInvokeRequest):
+def invoke_ai(body: AIInvokeRequest, user_id=Depends(get_current_user)):
     try:
         response = ai_service.invoke_text(body.model, body.text)
         logger.info('AI invoked successfully')
@@ -22,7 +23,7 @@ def invoke_ai(body: AIInvokeRequest):
 
 # POST /ai/ask-pdf
 @router.post("/ask-pdf", response_description="ask a question about a pdf to the AI model", status_code=200)
-def ask_pdf(query: str):
+def ask_pdf(query: str, user_id=Depends(get_current_user)):
     try:
         response = ai_service.ask_pdf(query)
         logger.info('AI asked pdf successfully')
