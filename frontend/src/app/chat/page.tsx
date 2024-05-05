d3fa0023ba96fa0974f76761c291fcf448d9dd25
gz244 <gz244@cornell.edu>
2024-05-05 19:11:58 -0400

    set up frontend chatting

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -8,26 +8,14 @@ import SendIcon from '@mui/icons-material/Send';
 import IconButton from '@mui/material/IconButton';
 import { PdfUpload } from '../apis/common/apis';
 import { invoke, askPdf } from '../apis/ai/apis';
-import swal from 'sweetalert';
-
-
-const exampleConversationHistory = [
-    { id: 1, username: 'Agent', message: "It was said that you would, destroy the Sith, not join them." },
-    { id: 2, username: 'Tom', message: "It was you who would bring balance to the Force" },
-    { id: 3, username: 'Agent', message: "Not leave it in Darkness" },
-    { id: 4, username: 'Tom', message: "I hate you!" },
-    { id: 5, username: 'Agent', message: "Liar!" },
-    { id: 6, username: 'Tom', message: "You're with him! You brought him here to kill me!" },
-    { id: 7, username: 'Agent', message: "No!" },
-    { id: 8, username: 'Tom', message: "Let her go, Anakin!" },
-    { id: 9, username: 'Agent', message: "Anakin, Anakin, Anakin!" },
-    { id: 10, username: 'Tom', message: "You turned her against me!" },
-];
+import ReactMarkdown from 'react-markdown';
+import { TypingEffect } from './typing.effect';
 
 
 export default function Chat() {
     const [pdf, setPdf] = useState<File | null>(null);
     const [query, setQuery] = useState<string>('');
+    const [conversationHistory, setConversationHistory] = useState<any[]>([]);
 
 
     const handleFileSelect = (e: any) => {
@@ -37,15 +25,20 @@ export default function Chat() {
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
+        const userEntry = { id: conversationHistory.length + 1, username: 'Tom', message: query };
+        setConversationHistory(prevHistory => [...prevHistory, userEntry]);
+        setQuery('');
 
         // ask question
         if (!pdf && query) {
             const response = await invoke({ model: "gpt-4-turbo", text: query });
-            swal('Response', response.response, 'success');
+            const responseEntry = { id: conversationHistory.length + 2, username: 'Agent', message: response.response };
+            setConversationHistory(prevHistory => [...prevHistory, responseEntry]);
         } else if (pdf && query) {
             await PdfUpload(pdf);
             const response = await askPdf(query);
-            swal('Response', response.answer, 'success');
+            const responseEntry = { id: conversationHistory.length + 2, username: 'Agent', message: response.sources[0].page_content };
+            setConversationHistory(prevHistory => [...prevHistory, responseEntry]);
         }
     }
 
@@ -55,46 +48,71 @@ export default function Chat() {
 
             <div className={`w-full min-h-screen flex flex-col justify-center items-center p-10 bg-transparent relative`}>
 
-                {/* static background 3d */}
-                <div className='w-full h-[30rem]'>
-                    <Spline scene="https://prod.spline.design/aIEbPZZVOB4Pm0PJ/scene.splinecode" style={{zIndex: -1}} />
-                </div>
+                <div className='w-full h-full flex flex flex-col justify-center items-center bg-transparent relatve hidden'>
+                    {/* static background 3d */}
+                    <div className='w-full h-[30rem]'>
+                        <Spline scene="https://prod.spline.design/aIEbPZZVOB4Pm0PJ/scene.splinecode" style={{zIndex: -1}} />
+                    </div>
 
-                {/* initial chat */}
-                <form onSubmit={handleSubmit} className={`w-2/4 h-[4rem] min-w-[25rem] flex flex-row items-center justify-center px-4 gap-2 rounded-lg z-10 ${styles['custom-glass-card']}`}>
-                    <AttachFileIcon className="text-white cursor-pointer" onClick={() => {
-                        const fileInput = document.getElementById('fileInput');
-                        if (fileInput) { fileInput.click();}
-                    }} />
-                    <input type="file" id="fileInput" className='hidden' onChange={handleFileSelect} />
-                    <input 
-                        type="text" 
-                        placeholder="Message your agent..." 
-                        className="input bg-transparent w-full"
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                    <IconButton type="submit">
-                        <SendIcon className="text-white" />
-                    </IconButton>
-                </form>
+                    {/* initial chat input box */}
+                    <form onSubmit={handleSubmit} className={`w-3/4 h-[3.5rem] min-w-[25rem] flex flex-row items-center justify-center px-4 gap-2 rounded-lg z-10 ${styles['custom-glass-card']}`}>
+                        <AttachFileIcon className="text-white cursor-pointer" onClick={() => {
+                            const fileInput = document.getElementById('fileInput');
+                            if (fileInput) { fileInput.click();}
+                        }} />
+                        <input type="file" id="fileInput" className='hidden' onChange={handleFileSelect} />
+                        <input 
+                            type="text" 
+                            placeholder="Message your agent..." 
+                            className="input bg-transparent w-full"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <IconButton type="submit">
+                            <SendIcon className="text-white" />
+                        </IconButton>
+                    </form>
+                </div>
 
                 {/* chat history */}
-                <div className={`w-3/4 h-[30rem] min-w-[25rem] min-h-[20rem] items-center justify-center p-6 rounded-lg z-10 ${styles['custom-glass-card']} hidden`}>
+                <>
+                    <div className={`w-3/4 h-5/6 min-h-[30rem] min-w-[25rem] min-h-[20rem] items-center justify-center p-6 rounded-lg z-10 bg-transport}`}>
 
-                    {/* chat area */}
-                    <div className='h-full w-full overflow-y-auto flex flex-col'>
-                        {exampleConversationHistory.map((chat, index) => (
-                            <div key={chat.id} className={`chat ${chat.username === 'Agent' ? 'chat-start' : 'chat-end'}`}>
-                                <div className="chat-image avatar">
-                                    <div className="w-10 rounded-full bg-white">
+                        {/* chat area */}
+                        <div className='h-full w-full overflow-y-auto flex flex-col gap-2 px-2'>
+                            {conversationHistory.map((chat, index) => (
+                                <div key={chat.id} className={`chat ${chat.username === 'Agent' ? 'chat-start' : 'chat-end'}`}>
+                                    <div className="chat-image avatar">
+                                        <div className="w-10 rounded-full bg-white">
+                                        </div>
+                                    </div>
+                                    <div className={`chat-bubble text-base ${chat.username === 'Agent'? 'bg-slate-400 text-black': 'bg-red-300 text-black'}`}>
+                                        {chat.username === 'Agent' ? ( <TypingEffect text={chat.message} speed={20} /> ) : ( <ReactMarkdown>{chat.message}</ReactMarkdown> )}
                                     </div>
                                 </div>
-                                <div className="chat-bubble">{chat.message}</div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
-                </div>
+
+                    {/* chat input box */}
+                    <form onSubmit={handleSubmit} className={`w-3/4 h-[3.5rem] min-w-[25rem] mt-8 flex flex-row items-center justify-center px-4 gap-2 rounded-lg z-10 ${styles['custom-glass-card']}`}>
+                        <AttachFileIcon className="text-white cursor-pointer" onClick={() => {
+                            const fileInput = document.getElementById('fileInput');
+                            if (fileInput) { fileInput.click();}
+                        }} />
+                        <input type="file" id="fileInput" className='hidden' onChange={handleFileSelect} />
+                        <input 
+                            type="text" 
+                            placeholder="Message your agent..." 
+                            className="input bg-transparent w-full"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <IconButton type="submit">
+                            <SendIcon className="text-white" />
+                        </IconButton>
+                    </form>
+                </>
                 
             </div>
         </div>
