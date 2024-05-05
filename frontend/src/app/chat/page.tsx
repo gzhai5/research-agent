21656a293b15f290237b29eac611ff3b404e5daa
gz244 <gz244@cornell.edu>
2024-05-05 19:17:47 -0400

    modify the initial chat logic

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -48,71 +48,72 @@ export default function Chat() {
 
             <div className={`w-full min-h-screen flex flex-col justify-center items-center p-10 bg-transparent relative`}>
 
-                <div className='w-full h-full flex flex flex-col justify-center items-center bg-transparent relatve hidden'>
-                    {/* static background 3d */}
-                    <div className='w-full h-[30rem]'>
-                        <Spline scene="https://prod.spline.design/aIEbPZZVOB4Pm0PJ/scene.splinecode" style={{zIndex: -1}} />
+                {conversationHistory.length === 0 ? (
+                    <div className='w-full h-full flex flex flex-col justify-center items-center bg-transparent relatve'>
+                        {/* static background 3d */}
+                        <div className='w-full h-[30rem]'>
+                            <Spline scene="https://prod.spline.design/aIEbPZZVOB4Pm0PJ/scene.splinecode" style={{zIndex: -1}} />
+                        </div>
+
+                        {/* initial chat input box */}
+                        <form onSubmit={handleSubmit} className={`w-3/4 h-[3.5rem] min-w-[25rem] flex flex-row items-center justify-center px-4 gap-2 rounded-lg z-10 ${styles['custom-glass-card']}`}>
+                            <AttachFileIcon className="text-white cursor-pointer" onClick={() => {
+                                const fileInput = document.getElementById('fileInput');
+                                if (fileInput) { fileInput.click();}
+                            }} />
+                            <input type="file" id="fileInput" className='hidden' onChange={handleFileSelect} />
+                            <input 
+                                type="text" 
+                                placeholder="Message your agent..." 
+                                className="input bg-transparent w-full"
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                            />
+                            <IconButton type="submit">
+                                <SendIcon className="text-white" />
+                            </IconButton>
+                        </form>
                     </div>
+                ) : (
+                    <>
+                        <div className={`w-3/4 h-5/6 min-h-[30rem] min-w-[25rem] min-h-[20rem] items-center justify-center p-6 rounded-lg z-10 bg-transport}`}>
 
-                    {/* initial chat input box */}
-                    <form onSubmit={handleSubmit} className={`w-3/4 h-[3.5rem] min-w-[25rem] flex flex-row items-center justify-center px-4 gap-2 rounded-lg z-10 ${styles['custom-glass-card']}`}>
-                        <AttachFileIcon className="text-white cursor-pointer" onClick={() => {
-                            const fileInput = document.getElementById('fileInput');
-                            if (fileInput) { fileInput.click();}
-                        }} />
-                        <input type="file" id="fileInput" className='hidden' onChange={handleFileSelect} />
-                        <input 
-                            type="text" 
-                            placeholder="Message your agent..." 
-                            className="input bg-transparent w-full"
-                            value={query}
-                            onChange={(e) => setQuery(e.target.value)}
-                        />
-                        <IconButton type="submit">
-                            <SendIcon className="text-white" />
-                        </IconButton>
-                    </form>
-                </div>
-
-                {/* chat history */}
-                <>
-                    <div className={`w-3/4 h-5/6 min-h-[30rem] min-w-[25rem] min-h-[20rem] items-center justify-center p-6 rounded-lg z-10 bg-transport}`}>
-
-                        {/* chat area */}
-                        <div className='h-full w-full overflow-y-auto flex flex-col gap-2 px-2'>
-                            {conversationHistory.map((chat, index) => (
-                                <div key={chat.id} className={`chat ${chat.username === 'Agent' ? 'chat-start' : 'chat-end'}`}>
-                                    <div className="chat-image avatar">
-                                        <div className="w-10 rounded-full bg-white">
+                            {/* chat area */}
+                            <div className='h-full w-full overflow-y-auto flex flex-col gap-2 px-2'>
+                                {conversationHistory.map((chat, index) => (
+                                    <div key={chat.id} className={`chat ${chat.username === 'Agent' ? 'chat-start' : 'chat-end'}`}>
+                                        <div className="chat-image avatar">
+                                            <div className="w-10 rounded-full bg-white">
+                                            </div>
+                                        </div>
+                                        <div className={`chat-bubble text-base ${chat.username === 'Agent'? 'bg-slate-400 text-black': 'bg-red-300 text-black'}`}>
+                                            {chat.username === 'Agent' ? ( <TypingEffect text={chat.message} speed={20} /> ) : ( <ReactMarkdown>{chat.message}</ReactMarkdown> )}
                                         </div>
                                     </div>
-                                    <div className={`chat-bubble text-base ${chat.username === 'Agent'? 'bg-slate-400 text-black': 'bg-red-300 text-black'}`}>
-                                        {chat.username === 'Agent' ? ( <TypingEffect text={chat.message} speed={20} /> ) : ( <ReactMarkdown>{chat.message}</ReactMarkdown> )}
-                                    </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
-                    </div>
 
-                    {/* chat input box */}
-                    <form onSubmit={handleSubmit} className={`w-3/4 h-[3.5rem] min-w-[25rem] mt-8 flex flex-row items-center justify-center px-4 gap-2 rounded-lg z-10 ${styles['custom-glass-card']}`}>
-                        <AttachFileIcon className="text-white cursor-pointer" onClick={() => {
-                            const fileInput = document.getElementById('fileInput');
-                            if (fileInput) { fileInput.click();}
-                        }} />
-                        <input type="file" id="fileInput" className='hidden' onChange={handleFileSelect} />
-                        <input 
-                            type="text" 
-                            placeholder="Message your agent..." 
-                            className="input bg-transparent w-full"
-                            value={query}
-                            onChange={(e) => setQuery(e.target.value)}
-                        />
-                        <IconButton type="submit">
-                            <SendIcon className="text-white" />
-                        </IconButton>
-                    </form>
-                </>
+                        {/* chat input box */}
+                        <form onSubmit={handleSubmit} className={`w-3/4 h-[3.5rem] min-w-[25rem] mt-8 flex flex-row items-center justify-center px-4 gap-2 rounded-lg z-10 ${styles['custom-glass-card']}`}>
+                            <AttachFileIcon className="text-white cursor-pointer" onClick={() => {
+                                const fileInput = document.getElementById('fileInput');
+                                if (fileInput) { fileInput.click();}
+                            }} />
+                            <input type="file" id="fileInput" className='hidden' onChange={handleFileSelect} />
+                            <input 
+                                type="text" 
+                                placeholder="Message your agent..." 
+                                className="input bg-transparent w-full"
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                            />
+                            <IconButton type="submit">
+                                <SendIcon className="text-white" />
+                            </IconButton>
+                        </form>
+                    </>
+                )}
                 
             </div>
         </div>
