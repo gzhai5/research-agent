388c3529813e02317bf8ffa57c18d85546e6dd80
gzhai5 <78711562+gzhai5@users.noreply.github.com>
2024-05-07 13:31:58 -0400

    [FE] Quick Fix: RM Template Universities (#7)
    * rm template universities
    
    * add chat link logic when user login
    
    ---------
    
    Co-authored-by: gz244 <gz244@cornell.edu>

--- frontend/src/app/home/components/hero.tsx
+++ frontend/src/app/home/components/hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../styles.module.css';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
@@ -7,6 +7,8 @@ import { motion } from 'framer-motion';
 
 export default function HeroSection() {
     const version = `${process.env.NEXT_PUBLIC_VERSION}`;
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(localStorage.getItem('username') ? true : false);
+
 
     return (
         <motion.div 
@@ -35,7 +37,7 @@ export default function HeroSection() {
             </div>
 
             {/* get started button */}
-            <Link href="/auth/login">
+            <Link href={isLoggedIn ? '/chat' : '/auth/login'}>
                 <button className='flex flex-row gap-1 justify-center items-center bg-[#5E6AD2] px-5 py-3 rounded-xl hover:bg-[#6775eb] transform transition-transform duration-300 active:scale-90 focus:outline-none'>
                     <p className="text-[#F7F8F8] font-medium text-base">Get Started</p>
                     <svg width="17" height="17" viewBox="0 0 17 17" fill="#F7F8F8" role="img" focusable="false" aria-hidden="true"><path d="M5.46967 11.4697C5.17678 11.7626 5.17678 12.2374 5.46967 12.5303C5.76256 12.8232 6.23744 12.8232 6.53033 12.5303L10.5303 8.53033C10.8207 8.23999 10.8236 7.77014 10.5368 7.47624L6.63419 3.47624C6.34492 3.17976 5.87009 3.17391 5.57361 3.46318C5.27713 3.75244 5.27128 4.22728 5.56054 4.52376L8.94583 7.99351L5.46967 11.4697Z"></path></svg>
